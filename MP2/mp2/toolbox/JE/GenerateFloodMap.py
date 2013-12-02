1856c9d2713c1ad3bc47d575de04c9bb943e5bc9
Jennyl <estil.jennyl@yahoo.com>
2013-12-02 00:05:02 +0000

    Added SQLAlchemy.pyproj

--- MP2/mp2/toolbox/JE/GenerateFloodMap.py
+++ MP2/mp2/toolbox/JE/GenerateFloodMap.py
@@ -1,6 +1,5 @@
 import subprocess
 import os
-import psycopg2
 
 def GenerateFloodMap(tinFile, csvFile, dtmFile, timeSnaps, modelFolder):
     """Generates depth grid and flood extent shapefile from the model result."""
@@ -13,9 +12,14 @@ def GenerateFloodMap(tinFile, csvFile, dtmFile, timeSnaps, modelFolder):
     try:
         args = 'calculate1dflooddepth /i: ',dtmFile+','+tinFile+','+csvFile+','+timeSnaps, ' /o: ',outGridFile
         subprocess.call([isisMapper, args],shell=True)
-        outShapefile = GridToShapefile(outGridFile,modelFolder)
+        ts = timeSnaps.split(",")
+        if len(ts) == 1: # if only 1 timeSnap specified
+            outShapefile = GridToShapefile(outGridFile,modelFolder)
+        elif len(ts) > 1: # if generating floodmaps for several timeSnaps
+            for t in ts:
+                outGridFile = outDir+'\output_depth_TS'+t+'.asc'
+                outShapefile = GridToShapefile(outGridFile,modelFolder)
     except ValueError as e: print(e)
-
     # Possible exception: csv file open, throws an error
     if os.path.exists(outGridFile) and os.path.exists(outShapefile):
         isSuccessful = True; 
@@ -32,7 +36,9 @@ def GridToShapefile(gridFile, modelFolder):
     outDir = modelFolder+'\FloodMaps'
     if not os.path.exists(outDir):
         os.makedirs(outDir)
-    outShapefile = outDir+'\output_depth.shp'
+    fileName = os.path.splitext(os.path.basename(gridFile))[0]
+    print(fileName)
+    outShapefile = outDir+'\\'+fileName+'.shp'
     try:
         args = '-extractfloodextent /i: ',gridFile,' /o: ',outShapefile
         subprocess.call([isisMapper, args],shell=True)
@@ -45,7 +51,7 @@ def GridToShapefile(gridFile, modelFolder):
 tinFile = r"C:\temp\ISISMapper\xsection4_TIN.htn" 
 csvFile = r"C:\temp\ISISMapper\xsection4.csv" 
 dtmFile = r"C:\temp\ISISMapper\dtm_pilot_taipei.asc" 
-timeSnaps = "1" 
+timeSnaps = "1,2" 
 outFile = r"C:\temp\ISISMapper"
 output = GenerateFloodMap(tinFile, csvFile, dtmFile, timeSnaps, outFile)
 print(output)
---
+++ SQLAlchemy/SQLAlchemy_1.py
@@ -0,0 +1,8 @@
+from sqlalchemy import *
+
+# Create an SQL Engine object: "engine://user:password@host:port/database"
+db = create_engine('postgres://taiwan_user:taiwan@localhost:5432/taiwan')
+db.echo = False
+
+metadata = BoundMetaData(db)
+
